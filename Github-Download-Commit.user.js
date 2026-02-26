11b0952ab6f54489984e332cd5f380164901c100
Damos <ilinsky.danila@yandex.ru>
2026-02-26 20:08:56 +0300

    Remove comment regarding SVG

--- Github-Download-Commit.user.js
+++ Github-Download-Commit.user.js
@@ -14,7 +14,6 @@
 (function () {
     'use strict';
 
-    // Added style="pointer-events: none;" so the SVG doesn't block the button's mouse hover event
     const zipSvg = `<svg style="pointer-events: none;" width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M14 2H6C4.895 2 4 2.895 4 4V20C4 21.105 4.895 22 6 22H18C19.105 22 20 21.105 20 20V8L14 2Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
         <path d="M14 2V8H20" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
